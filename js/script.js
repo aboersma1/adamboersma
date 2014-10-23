daca1814bce83fedc9b99dc475fe24deaa599f39
aboersma1 <adam.boersma@gmail.com>
2014-10-23 11:17:56 -0600

    Navbar transparency
    Navbar is now transparent somewhat in 768px widths and above.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+	
+
 	var siteApp = angular.module('siteApp', []);
 
 	siteApp.controller('siteController', function() {
@@ -42,3 +44,18 @@
 			}
 		];
 	});
+
+
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = 10;
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset) {
+          $(".navbar-default").css('background-color', 'white');
+       } else {
+          $('.navbar-default').css('background-color', 'rgba(255,255,255,0.85)');
+       }
+   });
+});
